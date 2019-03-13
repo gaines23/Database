444f6ca0e390702b8c29834cd3e2ccdc802bcbbe
Melissa <gaines.melissa23@gmail.com>
2019-03-12 17:38:53 -0700

    Newest Updates to FKs

--- TABLE ALTERS FOR NCT.sql
+++ TABLE ALTERS FOR NCT.sql
@@ -1,5 +1,8 @@
 -- ALTER TABLES 
 
+USE master 
+GO
+
 USE CompanyTest
 GO
 
@@ -44,8 +47,6 @@ ALTER TABLE Employee
 ADD CONSTRAINT FKReturnIDEmp
 FOREIGN KEY (ReturnID) REFERENCES ReturnedSales(ReturnedID)
 
-
-
 -- Employee Job Garm
 
 ALTER TABLE EmpJobGarm
@@ -53,13 +54,20 @@ ADD CONSTRAINT FKHierarchyJG
 FOREIGN KEY (Hierarchy) REFERENCES Hierarchy(JobHierarchyID)
 
 ALTER TABLE EmpJobGarm
-ADD CONSTRAINT FKEmpJG
-FOREIGN KEY (EmpID) REFERENCES Employee(EmpID)
+ADD CONSTRAINT FKGarmActivityJG
+FOREIGN KEY (GarmActivityID) REFERENCES GarmActivity(GarmActivityID)
 
 ALTER TABLE EmpJobGarm
-ADD CONSTRAINT FKGarmActivityJG
-FOREIGN KEY (GarmActivity) REFERENCES GarmActivity(EmpID, SlotNum)
+ADD CONSTRAINT FKGarmDefJG
+FOREIGN KEY (GarmDefaultID) REFERENCES GarmDefaultValues(GarmDefaultID)
 
+ALTER TABLE EmpJobGarm
+ADD CONSTRAINT FKEmpidJG
+FOREIGN KEY (EmpID) REFERENCES Employee(EmpID)
+		ON DELETE CASCADE
+		ON UPDATE CASCADE
+
+/*
 ALTER TABLE EmpJobGarm
 ADD CONSTRAINT FKSlotNumJG
 FOREIGN KEY (SlotNum) REFERENCES GarmActivity(SlotNum)
@@ -70,26 +78,17 @@ FOREIGN KEY (Size) REFERENCES Employee(GarmSize)
 
 ALTER TABLE EmpJobGarm
 ADD CONSTRAINT FKGarmTypeJG
-FOREIGN KEY (GarmType) REFERENCES Employee(GarmType, EmpID)
+FOREIGN KEY (GarmType) REFERENCES Employee(GarmType)
 
 ALTER TABLE EmpJobGarm
 ADD CONSTRAINT FKGarmLengthJG
 FOREIGN KEY (GarmLength) REFERENCES Employee(GarmLength, EmpID)
+*/
 
-ALTER TABLE EmpJobGarm
-ADD CONSTRAINT FKEqEmpGarmsJG
-FOREIGN KEY (EqEmpGarms) REFERENCES GarmDefaultValues(EqEmpGarms, GarmDefaultID)
 
-ALTER TABLE EmpJobGarm
-ADD CONSTRAINT FKEmpidJG
-FOREIGN KEY (EmpID) REFERENCES Employee(EmpID)
-		ON DELETE CASCADE
-		ON UPDATE CASCADE
-
-/*
 -- Locker
 -- We'll come back to this and Device Options later
-
+/*
 ALTER TABLE
 
 ADD CONSTRAINT PKLockNum PRIMARY KEY (LockerNumber)
@@ -130,47 +129,58 @@ FOREIGN KEY (GarmCost) REFERENCES GarmDefaultValues(GarmCost)
 
 ALTER TABLE GarmDefaultValues
 ADD CONSTRAINT FKGarmManuDV
-FOREIGN KEY (GarmManu) REFERENCES Manufaturer(ManufacturerID)
+FOREIGN KEY (GarmManuID) REFERENCES Manufacturer(ManufacturerID)
 
 ALTER TABLE GarmDefaultValues
 ADD CONSTRAINT FKJobHierarchyIdDV
 FOREIGN KEY (JobHierarchyID) REFERENCES Hierarchy(JobHierarchyID)
 
 
-
 -- garm depreciation
+
 -- Added Max#OfCleanings as FK to calculate PercentOfDepreciation
+
 ALTER TABLE GarmDepreciation
-ADD CONSTRAINT FKEmpID
-FOREIGN KEY (EmpID) REFERENCES EMPLOYEE(EmpID)
+ADD CONSTRAINT FKEmpIDDG
+FOREIGN KEY (EmpID) REFERENCES Employee(EmpID)
+
+ALTER TABLE GarmDepreciation
+ADD CONSTRAINT FKGarmentIDDG
+FOREIGN KEY (EmpID, GarmentID) REFERENCES EmpJobGarm(EmpID, GarmentID)
+
+ALTER TABLE GarmDepreciation
+ADD CONSTRAINT FKGarmActivityIDDG
+FOREIGN KEY (GarmActivityID) REFERENCES GarmActivity(GarmActivityID)
+
 /*
 ALTER TABLE GarmDepreciation
 ADD CONSTRAINT FKTickeID
 FOREIGN KEY (TicketNumber) REFERENCES TicketCreate(TicketNumber)
 */
-ALTER TABLE GarmDepreciation
-ADD CONSTRAINT FKGarmentID
-FOREIGN KEY (GarmentNumber) REFERENCES EmpJobGarm(GarmentID)
 
+/*
 ALTER TABLE GarmDepreciation
 ADD CONSTRAINT FKGarmMaxCleaning
 FOREIGN KEY (GarmMaxNumOfCleanings) REFERENCES GarmDefaultValues(MaxNumOfCleanings, GarmDefaultID)
-
-
-
+*/
 
 -- pretagged garms
 
 ALTER TABLE PreTaggedGarments
-ADD CONSTRAINT FKPurchaseOrderNumPG
-FOREIGN KEY (PurchaseOrder) REFERENCES PurchaseOrder(PurchaseOrderID)
+ADD CONSTRAINT AccountingIDPT
+FOREIGN KEY (AccountingID) REFERENCES Accounting(AccountingID)
 
+-- Scan
+
+ALTER TABLE Scan
+ADD CONSTRAINT FKPreTaggedScan
+FOREIGN KEY (PreTaggedGarmID) REFERENCES PreTaggedGarments(PreTaggedGarmID)
 
 -- inventory
 
 ALTER TABLE Inventory
-ADD CONSTRAINT FKGarmentID
-FOREIGN KEY (GarmentID) REFERENCES EmpJobGarm(GarmentID)
+ADD CONSTRAINT FKGarmentIDInventory
+FOREIGN KEY (EmpID, GarmentID) REFERENCES EmpJobGarm(EmpID, GarmentID)
 
 ALTER TABLE Inventory
 ADD CONSTRAINT FKEmpIDInventory
@@ -178,18 +188,10 @@ FOREIGN KEY (EmpID) REFERENCES Employee(EmpID)
 
 ALTER TABLE Inventory
 ADD CONSTRAINT FKSlotNumInventory
-FOREIGN KEY (SlotNum) REFERENCES GarmActivity(SlotNum, PKGarmActivityID)
+FOREIGN KEY (GarmActivityID) REFERENCES GarmActivity(GarmActivityID)
 
-ALTER TABLE Inventory 
-ADD CONSTRAINT FKConveyorInventory
-FOREIGN KEY (Conveyor) REFERENCES GarmActivity(Conveyor, PKGarmActivityID)
-
-ALTER TABLE Inventory
-ADD CONSTRAINT FKGarmActivityInventory
-FOREIGN KEY (GarmActivityID) REFERENCES GarmActivity(PKGarmActivityID)
-
-ALTER TABLE Inventory
-ADD CONSTRAINT FKGarmDefaultIDInventory
+ALTER TABLE INVENTORY
+ADD CONSTRAINT FKGarmDefaultValue
 FOREIGN KEY (GarmDefaultID) REFERENCES GarmDefaultValues(GarmDefaultID)
 
 ALTER TABLE Inventory
@@ -197,15 +199,11 @@ ADD CONSTRAINT FKManufacturerIDInventory
 FOREIGN KEY (ManufacturerID) REFERENCES Manufacturer(ManufacturerID)
 
 ALTER TABLE Inventory
-ADD CONSTRAINT FKGarmCostInventory
-FOREIGN KEY (GarmCost) REFERENCES GarmDefaultValues(GarmCost, GarmDefaultValues)
+ADD CONSTRAINT FKScanIDInventory
+FOREIGN KEY(ScanID) REFERENCES Scan(ScanID)
+
 
 /*
-ALTER TABLE Inventory
-ADD CONSTRAINT FKAssignInventory
-FOREIGN KEY (AssignmentStatus) REFERENCES CleaningActivity(CleaningStatus)
-*/
-
 ALTER TABLE Inventory
 ADD CONSTRAINT FKBarcodeInventory
 FOREIGN KEY (Barcode) REFERENCES Scan(BARCODE)
@@ -213,6 +211,31 @@ FOREIGN KEY (Barcode) REFERENCES Scan(BARCODE)
 ALTER TABLE Inventory
 ADD CONSTRAINT FKRfidInventory
 FOREIGN KEY (RFID) REFERENCES Scan(RFID)
+*/
+
+/*
+ALTER TABLE Inventory 
+ADD CONSTRAINT FKConveyorInventory
+FOREIGN KEY (Conveyor) REFERENCES GarmActivity(Conveyor, PKGarmActivityID)
+
+ALTER TABLE Inventory
+ADD CONSTRAINT FKGarmActivityInventory
+FOREIGN KEY (GarmActivityID) REFERENCES GarmActivity(PKGarmActivityID)
+*/
+
+/*
+ALTER TABLE Inventory
+ADD CONSTRAINT FKGarmCostInventory
+FOREIGN KEY (GarmCost) REFERENCES GarmDefaultValues(GarmCost, GarmDefaultValues)
+*/
+
+/*
+ALTER TABLE Inventory
+ADD CONSTRAINT FKAssignInventory
+FOREIGN KEY (AssignmentStatus) REFERENCES CleaningActivity(CleaningStatus)
+*/
+
+
 
 
 -- garm activity
@@ -224,8 +247,19 @@ FOREIGN KEY (EmpID) REFERENCES Employee(EmpID)
 
 ALTER TABLE GarmActivity
 ADD CONSTRAINT FKGarmIDGAF
-FOREIGN KEY (GarmID) REFERENCES EmpJobGarm(EmpJobGarmID)
+FOREIGN KEY (EmpID, GarmentID) REFERENCES EmpJobGarm(EmpID, GarmentID)
 
+ALTER TABLE GarmActivity
+ADD CONSTRAINT FKScanIDGAF
+FOREIGN KEY (ScanID) REFERENCES Scan(ScanID)
+
+ALTER TABLE GarmActivity
+ADD CONSTRAINT FKCleaningIDGAF
+FOREIGN KEY (CleaningActivityID) REFERENCES CleaningActivity(CleaningActID)
+
+
+
+/*
 ALTER TABLE GarmActivity
 ADD CONSTRAINT FKrfidGA
 FOREIGN KEY (RFID) REFERENCES Inventory(RFID)
@@ -233,10 +267,8 @@ FOREIGN KEY (RFID) REFERENCES Inventory(RFID)
 ALTER TABLE GarmActivity
 ADD CONSTRAINT FKBarcodeGA
 FOREIGN KEY (Barcode) REFERENCES Inventory(Barcode)
+*/
 
-ALTER TABLE GarmActivity
-ADD CONSTRAINT FKCleaningIDGAF
-FOREIGN KEY (CleaningActivityID) REFERENCES CleaningActivity(CleaningActID)
 
 /* May have to be determined by PROC
 ALTER TABLE GarmActivity
@@ -248,14 +280,14 @@ FOREIGN KEY (Delinquent) REFERENCES CleaningActivity(DelinquentGarm)
 -- Added Delinquent Garms FK from Cleaning Activity
 
 ALTER TABLE MissingGarments -- need to think over CASCADE options later
-ADD CONSTRAINT GarmIDMG
-FOREIGN KEY (GarmID) REFERENCES EmpJobGarm(GarmentID)
+ADD CONSTRAINT FKGarmIDMG
+FOREIGN KEY (EmpID, GarmentID) REFERENCES EmpJobGarm(EmpID, GarmentID)
 
 ALTER TABLE MissingGarments
-ADD CONSTRAINT FKMissingDateTimeMG
-FOREIGN KEY (MissingDateTime) REFERENCES GarmActivity(Missing, GarmID)
+ADD CONSTRAINT FKEmpIDMG
+FOREIGN KEY (EmpID) REFERENCES Employee(EmpID)
 
-ALTER TABLE MissingGarments
+ALTER TABLE EmpJobGarm
 ADD CONSTRAINT FKGarmActivityMG
 FOREIGN KEY (GarmActivityID) REFERENCES GarmActivity(GarmActivityID)
 
@@ -264,6 +296,10 @@ ADD CONSTRAINT FKGarmDefaultValueMG
 FOREIGN KEY (GarmDefaultValueID) REFERENCES GarmDefaultValues(GarmDefaultID)
 
 /*
+ALTER TABLE MissingGarments
+ADD CONSTRAINT FKMissingDateTimeMG
+FOREIGN KEY (MissingDateTime) REFERENCES GarmActivity(Missing, GarmID)
+
 ALTER TABLE MissingGarments
 ADD CONSTRAINT FKDelinquentMG
 FOREIGN KEY (DelinquentGarm) REFERENCES GarmActivity(DelinquentGarm)
@@ -275,15 +311,19 @@ FOREIGN KEY (DelinquentGarm) REFERENCES GarmActivity(DelinquentGarm)
 -- deleted garms
 
 ALTER TABLE DeletedGarments
-ADD CONSTRAINT FKGarmIDdg
-FOREIGN KEY (GarmID) REFERENCES Inventory(GarmentID)
+ADD CONSTRAINT FKGarmIDDelGarm
+FOREIGN KEY (EmpID, GarmentID) REFERENCES EmpJobGarm(EmpID, GarmentID)
 
 ALTER TABLE DeletedGarments
-ADD CONSTRAINT FKMissingGarmDG
+ADD CONSTRAINT FKEmpIDDelGarm
+FOREIGN KEY (EmpID) REFERENCES Employee(EmpID)
+
+ALTER TABLE DeletedGarments
+ADD CONSTRAINT FKMissingGarmDelGarm
 FOREIGN KEY (MissingGarmID) REFERENCES MissingGarments(MissingGarmID)
 
 ALTER TABLE DeletedGarments
-ADD CONSTRAINT FKNonTaggedDG
+ADD CONSTRAINT FKNonTaggedDelGarm
 FOREIGN KEY (NontaggedID) REFERENCES NonTaggedGarm(NonTaggedGarmID)
 
 
@@ -294,7 +334,19 @@ FOREIGN KEY (NontaggedID) REFERENCES NonTaggedGarm(NonTaggedGarmID)
 
 ALTER TABLE NonTaggedGarm
 ADD CONSTRAINT FKGarmNT
-FOREIGN KEY (Garm) REFERENCES Inventory(GarmentID)
+FOREIGN KEY (EmpID, GarmentID) REFERENCES EmpJobGarm(EmpID, GarmentID)
+
+ALTER TABLE EmpJobGarm
+ADD CONSTRAINT FKGarmActivityNT
+FOREIGN KEY (GarmActivityID) REFERENCES GarmActivity(GarmActivityID)
+
+ALTER TABLE NonTaggedGarm
+ADD CONSTRAINT FKEmpIDNT
+FOREIGN KEY (EmpID) REFERENCES Employee(EmpID)
+
+ALTER TABLE NonTaggedGarm
+ADD CONSTRAINT FKInventoryIDNT
+FOREIGN KEY(InventoryID) REFERENCES Inventory(InventoryID)
 
 -- What was my thought process behind the assigned column?
 -- Does it even make sense?
@@ -303,54 +355,76 @@ FOREIGN KEY (Garm) REFERENCES Inventory(GarmentID)
 ALTER TABLE NonTaggedGarm
 ADD CONSTRAINT FKAssignedNT
 FOREIGN KEY (Assigned) REFERENCES GarmActivity(Assigned)
-*/
-
-ALTER TABLE NonTaggedGarm
-ADD CONSTRAINT FKEmpIDNT
-FOREIGN KEY (EmpID) REFERENCES Employee(EmpID)
 
 ALTER TABLE NonTaggedGarm
 ADD CONSTRAINT FKLoanerNT
 FOREIGN KEY (Loaned) REFERENCES GarmActivity(Loaned)
 
+
 ALTER TABLE NonTaggedGarm
 ADD CONSTRAINT FKGarmLocation
 FOREIGN KEY (GarmLocation) REFERENCES Inventory(GarmLocation)
 
-
+*/
  
 -- accounting
 
 ALTER TABLE Accounting
-ADD CONSTRAINT FKPurchaseOrderIDNT
+ADD CONSTRAINT FKPurchaseOrderIDAcct
 FOREIGN KEY (PurchaseOrderID) REFERENCES PurchaseOrder(PurchaseOrderID)
 
 ALTER TABLE Accounting
-ADD CONSTRAINT FKEmpOrderNT
-FOREIGN KEY (EmpOrder) REFERENCES EmpOrder(EmpOrderID)
+ADD CONSTRAINT FKEmpOrderAcct
+FOREIGN KEY (EmpOrderID) REFERENCES EmpOrder(EmpOrderID)
+
+ALTER TABLE Accounting
+ADD CONSTRAINT FKSalesIDAcct
+FOREIGN KEY (SalesID) REFERENCES Sales(SalesID)
+
+ALTER TABLE Accounting
+ADD CONSTRAINT FKManuIDAcct
+FOREIGN KEY (ManufacturerID) REFERENCES Manufacturer(ManufacturerID)
 
 
 
 -- sales
 
 ALTER TABLE Sales
+ADD CONSTRAINT FKEmpJobGarmSales
+FOREIGN KEY (EmpID, GarmentID) REFERENCES EmpJobGarm(EmpID, GarmentID)
+
+ALTER TABLE Sales
 ADD CONSTRAINT FKInventorySales
-FOREIGN KEY (GarmentID) REFERENCES Inventory(GarmentID)
+FOREIGN KEY (InventoryID) REFERENCES Inventory(InventoryID)
+
+ALTER TABLE Sales
+ADD CONSTRAINT FKEmpIDSales
+FOREIGN KEY(EmpID) REFERENCES Employee(EmpID)
 
 ALTER TABLE Sales
 ADD CONSTRAINT FKPurchaseOrderSales
 FOREIGN KEY (PurchaseOrderID) REFERENCES PurchaseOrder(PurchaseOrderID)
 
 ALTER TABLE Sales
-ADD CONSTRAINT FKEmpOrderSales
-FOREIGN KEY (EmpOrderID) REFERENCES EmpOrder(EmpOrderID)
+ADD CONSTRAINT FKGarmentDefaultValuesSales
+FOREIGN KEY (GarmDefaultID) REFERENCES GarmDefaultValues(GarmDefaultID)
 
 ALTER TABLE Sales
-ADD CONSTRAINT FKGarmentDefaultValuesSales
-FOREIGN KEY (GarmentDefaultValue) REFERENCES GarmDefaultValues(GarmDefaultID)
+ADD CONSTRAINT FKAccountingSales
+FOREIGN KEY (AccountingID) REFERENCES Accounting(AccountingID)
+
+ALTER TABLE Sales
+ADD CONSTRAINT FKManuIDSales
+FOREIGN KEY (ManufacturerID) REFERENCES Manufacturer(ManufacturerID)
+
+
 -- MAYBE USE GARM COST? might not be specific enough.
 
-
+/*
+ALTER TABLE Sales
+ADD CONSTRAINT FKEmpOrderSales
+FOREIGN KEY (EmpOrderID) REFERENCES EmpOrder(EmpOrderID)
+*/
 
 --returned sales
 
@@ -370,21 +444,31 @@ ALTER TABLE ReturnedSales
 ADD CONSTRAINT FKEmpRS
 FOREIGN KEY (EmpID) REFERENCES Employee(EmpID)
 
+/*
 ALTER TABLE ReturnedSales
 ADD CONSTRAINT FKGarmIDRS
-FOREIGN KEY (GarmID) REFERENCES EmpOrder
-
+FOREIGN KEY (GarmID) REFERENCES EmpOrder(GarmID)
+*/
 
 -- purchase order
 
 ALTER TABLE PurchaseOrder
 ADD CONSTRAINT FKGarmDefaultPO
-FOREIGN KEY (GrmentDefaultValue) REFERENCES GarmDefaultValue(GarmDefaultID)
+FOREIGN KEY (GarmentDefaultValue) REFERENCES GarmDefaultValues(GarmDefaultID)
 
+ALTER TABLE PurchaseOrder
+ADD CONSTRAINT FKAccountingPO
+FOREIGN KEY (AccountingID) REFERENCES Accounting(AccountingID)
+
+ALTER TABLE PurchaseOrder
+ADD CONSTRAINT FKManuIDPO
+FOREIGN KEY (ManufacturerID) REFERENCES Manufacturer(ManufacturerID)
+
+/*
 ALTER TABLE PurchaseOrder
 ADD CONSTRAINT FKGarmSizePO
 FOREIGN KEY (Garmsize) REFERENCES GarmDefaultValue(GarmSize, GarmDefaultID)
-
+*/
 
 
 -- emporder
@@ -394,9 +478,10 @@ ADD CONSTRAINT FKEmpIDEO
 FOREIGN KEY (EmpID) REFERENCES Employee(EmpID)
 
 ALTER TABLE EmpOrder
-ADD CONSTRAINT FKHierarchy
+ADD CONSTRAINT FKHierarchyEO
 FOREIGN KEY (Hierarchy) REFERENCES Hierarchy(JobHierarchyID)
 
+/*
 ALTER TABLE EmpOrder
 ADD CONSTRAINT FKbarcodeEO
 FOREIGN KEY (Barcode) REFERENCES Inventory(Barcode)
@@ -408,6 +493,8 @@ FOREIGN KEY (RFID) REFERENCES Inventory(RFID)
 ALTER TABLE EmpOrder
 ADD CONSTRAINT FKGarmDepPerct
 FOREIGN KEY (GarmDeprecPercentage) REFERENCES GarmDepreciation(PercentageOfDepreciation, GarmentID)
+*/
+
 -- Added this as new FK
 -- Once this reaches ~90%, new order is set into motion
 
@@ -415,12 +502,24 @@ FOREIGN KEY (GarmDeprecPercentage) REFERENCES GarmDepreciation(PercentageOfDepre
 -- CleaningActivity
 
 ALTER TABLE CleaningActivity
-ADD CONSTRAINT FKCleanerCA
-FOREIGN KEY (CleanerName) REFERENCES CleanerInfo(CleanerName, CleanerID)
+ADD CONSTRAINT FKCleanerIDCA
+FOREIGN KEY (CleanerID) REFERENCES CleanerInfo(CleanerID)
+
+ALTER TABLE CleaningActivity
+ADD CONSTRAINT FKEmpIDCA
+FOREIGN KEY (EmpID) REFERENCES Employee(EmpID)
 
 ALTER TABLE CleaningActivity
 ADD CONSTRAINT FKGarmCA
-FOREIGN KEY (GarmID) REFERENCES GarmActivity(GarmID)
+FOREIGN KEY (GarmActivityID) REFERENCES GarmActivity(GarmActivityID)
+
+/*
+
+ALTER TABLE CleaningActivity
+ADD CONSTRAINT FKCleanerNameCA
+FOREIGN KEY (CleanerName) REFERENCES CleanerInfo(CleanerName, CleanerID)
+
+
 
 ALTER TABLE CleaningActivity
 ADD CONSTRAINT FKrfidCA
@@ -429,6 +528,8 @@ FOREIGN KEY (RFID) REFERENCES Inventory(RFID)
 ALTER TABLE CleaningActivity
 ADD CONSTRAINT FKBarcodeCA
 FOREIGN KEY (Barcode) REFERENCES Inventory(Barcode)
+
+*/
 
 
 
@@ -440,4 +541,12 @@ FOREIGN KEY (EmpID) REFERENCES Employee(EmpID)
 
 ALTER TABLE Repairs
 ADD CONSTRAINT FKGarmRepair
-FOREIGN KEY (Garm) REFERENCES GarmentDefaultValue(GarmDefaultID)
+FOREIGN KEY (EmpID, GarmentID) REFERENCES EmpJobGarm(EmpID, GarmentID)
+
+ALTER TABLE Repairs
+ADD CONSTRAINT FKCkeaningActivityRepair
+FOREIGN KEY (CleaningActivityID) REFERENCES CleaningActivity(CleaningActID)
+
+ALTER TABLE Repairs
+ADD CONSTRAINT FKGarmActivityRepair
+FOREIGN KEY (GarmActivityID) REFERENCES GarmActivity(GarmActivityID)
